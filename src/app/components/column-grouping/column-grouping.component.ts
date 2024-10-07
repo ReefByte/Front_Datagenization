909239e7a4248d528900e074a4cd1aa34a45d7da
paula_penuela <paula.penuela@procontacto.com.mx>
2024-10-06 19:31:51 -0500

    colores

--- src/app/components/column-grouping/column-grouping.component.ts
+++ src/app/components/column-grouping/column-grouping.component.ts
@@ -1,39 +1,41 @@
 import { Component } from '@angular/core';
-import {UploadService} from "../../service/upload.service";
-import {ColumnGroupingService} from "../../service/column-grouping.service";
+import { UploadService } from '../../service/upload.service';
+import { ColumnGroupingService } from '../../service/column-grouping.service';
 
 @Component({
   selector: 'app-column-grouping',
   templateUrl: './column-grouping.component.html',
-  styleUrls: ['./column-grouping.component.css']
+  styleUrls: ['./column-grouping.component.css'],
 })
 export class ColumnGroupingComponent {
+  selectionRows: Array<{ [key: string]: string[] }> = [];
+  session_id: string = '';
+  columns: { [key: string]: string[] } = {};
 
-  selectionRows:  Array<{ [key: string]: string[] }> = [];
-  session_id:string="";
-  columns: { [key:string]:string[] }= {};
-
-  constructor(private uploadService:UploadService, private columnGroupingService:ColumnGroupingService) {}
+  constructor(
+    private uploadService: UploadService,
+    private columnGroupingService: ColumnGroupingService
+  ) {}
 
   ngOnInit(): void {
     this.session_id = this.uploadService.getSession_id;
     this.getColumns();
   }
 
-  getColumns(){
-    if(this.session_id){
+  getColumns() {
+    if (this.session_id) {
       this.columnGroupingService.getColumns(this.session_id).subscribe(
-        (response:any) =>{
+        (response: any) => {
           this.columns = response;
           this.initializeSelectionRow();
-          console.log("Datos guardados exitosamente ", this.columns);
+          console.log('Datos guardados exitosamente ', this.columns);
         },
-        (error:any) =>{
-          console.error("Error al obtener datos: ",error)
+        (error: any) => {
+          console.error('Error al obtener datos: ', error);
         }
-      )
-    }else{
-      console.error("No hay session id valido")
+      );
+    } else {
+      console.error('No hay session id valido');
     }
   }
 
@@ -51,19 +53,23 @@ export class ColumnGroupingComponent {
 
   addSelection(rowIndex: number, fileName: string): void {
     if (this.selectionRows[rowIndex][fileName]) {
-      this.selectionRows[rowIndex][fileName].push('');  // Añadir una nueva selección vacía
+      this.selectionRows[rowIndex][fileName].push(''); // Añadir una nueva selección vacía
     }
   }
 
   addRow(): void {
-    this.initializeSelectionRow();  // Usa la función para agregar una nueva fila inicializada
+    this.initializeSelectionRow(); // Usa la función para agregar una nueva fila inicializada
   }
 
   processGrouping(): void {
-    console.log("Procesando la agrupación:", this.selectionRows);
+    console.log('Procesando la agrupación:', this.selectionRows);
     //TODO: ENVIAR INFO AL BACK
   }
-  removeSelection(rowIndex: number, fileName: string, selectionIndex: number): void {
+  removeSelection(
+    rowIndex: number,
+    fileName: string,
+    selectionIndex: number
+  ): void {
     if (this.selectionRows[rowIndex][fileName].length > 1) {
       this.selectionRows[rowIndex][fileName].splice(selectionIndex, 1);
     } else {
